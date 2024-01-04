6867f7e4bbcdb22ced3ba1b05864e341484bc185
sujith surendran <sujithsurendran4433@gmail.com>
2024-01-04 20:08:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// write a program for printing a numbers from 1 to 10
+
+// for (let i=1; i<=10; i++){
+//     console.log("number is ",i)
+// }
+
+// create an array with 5 elements and print all the elements in the array 
+
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]); 
+// }
+// let arr = [1,2,3,4,5];
+// let i=0;
+// while ( i<arr.length) {
+//     console.log(arr[i]);
+//     i++;
+// }
+
+// let person = {
+//     name:'sujith',
+//     age:21,
+//     gender:'male'
+// }
+
+// for(item in person){
+//     console.log(person[item]);
+// }
+
+let arr = [1,2,3,4,5,6];
+ let i=0;
+
+ do {
+    console.log(i);
+    i++;
+ } while ( i<arr.length)
