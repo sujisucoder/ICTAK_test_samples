c8c67c0bd162bdf4f8a5a95fd34d8114944f05c8
sujith surendran <sujithsurendran4433@gmail.com>
2024-01-08 18:24:44 +0530

    Questions 1 and 3 completed

--- index.js
+++ index.js
@@ -1,3 +1,62 @@
+//1. Write a JavaScript function to get an array and to read the first element of an array. Check whether it is prime or not. 
+
+// let numArray = [11,6,2,7,71]
+
+// function primeCheck(numbers) {
+//    console.log(numbers);
+//    let i=0;
+//    while(i<numArray.length){
+//       if(numArray[i]==2||numArray[i]%2!=0){
+//          console.log("prime number",numArray[i]);
+
+//       }else{
+//          console.log("not prime number",numArray[i]);
+
+//       }
+//       i++;
+//    }
+// }
+// primeCheck(numArray);
+
+
+
+//2. Write a JavaScript program to find the most frequent item of an array
+
+// let array = [1, 2, 2, 3, 4, 4, 4, 5, 5, 5, 5];
+
+
+//3. Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, check if the current number is odd or even, and display the message to the screen as odd or even.
+
+// function oddEvenCheck() {
+//    for (let i = 0; i <= 15; i++) {
+//       if (i%2==0) {
+//          console.log(i,"is even");
+//       } else {
+//          console.log(i,"is odd");
+         
+//       }      
+//    }
+// }
+// oddEvenCheck();
+
+
+//4. Write a JavaScript program to find the sum of squares of the elements of an array.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // write a program for printing a numbers from 1 to 10
 
 // for (let i=1; i<=10; i++){
@@ -28,10 +87,10 @@
 //     console.log(person[item]);
 // }
 
-let arr = [1,2,3,4,5,6];
- let i=0;
+// let arr = [1,2,3,4,5,6];
+//  let i=0;
 
- do {
-    console.log(i);
-    i++;
- } while ( i<arr.length)
+//  do {
+//     console.log(i);
+//     i++;
+//  } while ( i<arr.length)
